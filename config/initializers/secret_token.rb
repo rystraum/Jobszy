90e767571357a20c7bf15417a32ec21bd31bcd2a
Gavin Hughes <gavhug@gmail.com>
2011-10-18 21:58:56 +0800

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jobszy::Application.config.secret_token = '79b1634a0df1bfc0b882e18cb9dffd2507e34a922b1c4801e5f11da223702b5cc30cc890af5dd97fad3da1db45cdba65e52c3ae8fc0031ea71a2ca57c907e77e'
